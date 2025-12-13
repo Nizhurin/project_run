5a2c99bae3251c55f3e902dc39311283dcafa641
Viktor Nizhurin <nizhurin@gmail.com>
2025-12-13 16:47:30 +0200

    add UserSerializer, add UserViewSet and user router

--- app_run/views.py
+++ app_run/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
 
-from app_run.models import Run
-from app_run.serializers import RunSerializer
+from app_run.models import Run, User
+from app_run.serializers import RunSerializer, UserSerializer
 
 
 @api_view(['GET'])
@@ -20,3 +20,18 @@ def company_details(request):
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all().exclude(is_superuser=True)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = self.queryset  # Используем базовый queryset определенный выше, на уровне класса
+        user_type = self.request.query_params.get('type', None)  # Получим параметр type
+        is_stuff = False
+        if user_type:
+            if user_type == 'coach':
+                is_stuff = True
+            qs = qs.filter(is_staff=is_stuff)
+        return qs
