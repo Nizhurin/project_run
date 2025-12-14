716e461e157504c8fdfb40f2e05d9e704c7b77d4
Viktor Nizhurin <nizhurin@gmail.com>
2025-12-14 12:14:50 +0200

    add athlete_data for api /api/runs/ (add new AthleteSerializer and change RunViewSet

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -2,7 +2,15 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Run
 
+
+class AthleteDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'last_name', 'first_name']
+
 class RunSerializer(serializers.ModelSerializer):
+    athlete_data = AthleteDataSerializer(source='athlete', read_only=True)
+
     class Meta:
         model = Run
         fields = '__all__'
